8247d6b890f3fbabe48f2b2005301fc6d55de697
Júnior Garcia <itamarjr91@gmail.com>
2016-11-15 12:47:18 -0300

    Changed app's namespace and fixed bug when Twig wanted to create cache.

--- app/config.php
+++ app/config.php
@@ -1,8 +1,9 @@
 <?php
 
-use PHPAppGenerator\Assets;
-
 /******************************************** Loading the PHP dependencies. *******************************************/
+use PHPWebAppGenerator\Assets;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 /*************************************** Initializing the environment variables ***************************************/
@@ -21,9 +22,10 @@ $app->register(new \Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/templates',
 ));
 
-// Enabling Twig cache just if the environment is development
+// Enabling Twig cache just for production environment. By default we write on temporary system's folder to avoid
+// permission issues.
 if (!DEBUG) {
-    $app['twig']->setCache(__DIR__ . '/cache');
+    $app['twig']->setCache(sys_get_temp_dir() . '/cache');
 }
 
 // Configuring the globals for Twig. These can be overridden
