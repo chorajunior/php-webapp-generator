f991f8114334898d21defc9f662f25093133ee90
Júnior Garcia <itamarjr91@gmail.com>
2017-03-08 13:35:06 -0300

    New structure added do the application: /public is now the public folder; .gitignore updated; Reconfigured functions to the new asset structure.

--- app/config.php
+++ app/config.php
@@ -2,17 +2,17 @@
 
 /******************************************** Loading the PHP dependencies. *******************************************/
 use PHPWebAppGenerator\Assets;
-use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 /*************************************** Initializing the environment variables ***************************************/
 $dotenv = new \Dotenv\Dotenv($_SERVER['DOCUMENT_ROOT']);
 $dotenv->load();
-$dotenv->required(['ENV', 'APP_TITLE']);
+$dotenv->required(array('ENV', 'APP_TITLE'));
 
 // Setting the debug mode based on the current environment
-define('DEBUG', getenv('ENV') !== 'production');
+// define('DEBUG', getenv('ENV') !== 'production');
+define('DEBUG', true);
 
 /************************** Initializing the router using Silex: http://silex.sensiolabs.org/ *************************/
 $app = new Silex\Application(array('debug' => DEBUG));
@@ -22,27 +22,32 @@ $app->register(new \Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/templates',
 ));
 
-// Enabling Twig cache just for production environment. By default we write on temporary system's folder to avoid
-// permission issues.
-if (!DEBUG) {
-    $app['twig']->setCache(sys_get_temp_dir() . '/cache');
-}
-
 // Configuring the globals for Twig. These can be overridden
+/** @var $twig Twig_Environment */
 $twig = $app['twig'];
 $twig->addGlobal('appTitle', getenv('APP_TITLE'));
 
+// Enabling Twig cache just for production environment. By default we write on temporary system's folder to avoid
+// permission issues.
+if (!DEBUG) {
+    $twig->setCache(sys_get_temp_dir());
+}
+
 // Adding assets functions to Twig Environment
 $assets = new Assets();
-$twig->addFunction(new Twig_SimpleFunction('script', function($script) {
-    echo Assets::getScript($script);
-}));
-$app['twig']->addFunction('style', new Twig_SimpleFunction('style', function($style) {
-    echo Assets::getStyle($style);
-}));
-$app['twig']->addFunction('image', new Twig_SimpleFunction('image', function($image) {
-    echo Assets::getImage($image);
-}));
-$app['twig']->addFunction('script', new Twig_SimpleFunction('script', function($script) {
-    echo Assets::getScript($script);
-}));
+$appFunctions = array(
+    'script' => new Twig_SimpleFunction('script', function($script) {
+        echo Assets::getScript($script);
+    }),
+    'style' => new Twig_SimpleFunction('style', function($style) {
+        echo Assets::getStyle($style);
+    }),
+    'image' => new Twig_SimpleFunction('image', function($image) {
+        echo Assets::getImage($image);
+    }),
+);
+
+// Iterating in the array of the Twig custom functions.
+foreach($appFunctions as $fnName => $fnObj) {
+    $twig->addFunction($fnName, $fnObj);
+}
